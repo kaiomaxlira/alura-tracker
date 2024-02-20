ec90321a76d22beee7f3794c2f331e4efc1c3c0c
Kaio Max <kaiomax01@gmail.com>
2024-02-20 14:14:03 -0300

    Adicinando lista

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,39 @@
 <template>
-<main class="columns is-gapless is-multiline">
+  <main class="columns is-gapless is-multiline">
+    <div class="column is-one-quarter">
+      <BarraLateral />
+    </div>
 
-  <div class="column is-one-quarter">
-    <BarraLateral />
-  </div>
-  
-  <div class="column is-three-quarter">
-    <Formulario />
-    
-  </div>
+    <div class="column is-three-quarter">
+      <Formulario />
 
-</main>
+      <div class="lista">
+        <Tarefa />
+        <Tarefa />
+        <Tarefa />
+      </div>
+    </div>
+  </main>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import BarraLateral from './components/BarraLateral.vue'
-import Formulario from './components/Formulario.vue'
+import { defineComponent } from "vue";
+import BarraLateral from "./components/BarraLateral.vue";
+import Formulario from "./components/Formulario.vue";
+import Tarefa from "./components/Tarefa.vue";
 
 export default defineComponent({
-  name: 'App',
+  name: "App",
   components: {
-  BarraLateral,
-  Formulario
-}});
-
+    BarraLateral,
+    Formulario,
+    Tarefa,
+  },
+});
 </script>
 
 <style>
-
+.lista {
+  padding: 1.25rem;
+}
 </style>
---
+++ src/components/Tarefa.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="box has-text-weight-bold">
+    <div class="columns">
+      <div class="column is-7">Descrição da tafera</div>
+      <div class="column">
+        <Cronometro tempoEmSegundos="15" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import Cronometro from "./Cronometro.vue"
+
+export default defineComponent({
+    name: 'App',
+    components: {
+       Cronometro
+    }
+})
+</script>
+
+<style scoped>
+.box {
+    background: #9124;
+}
+
+</style>
