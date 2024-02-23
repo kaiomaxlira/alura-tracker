2a32be59a146a53c1f9cec7667562ca8b24819c3
Kaio Max <kaiomax01@gmail.com>
2024-02-23 14:18:01 -0300

    Adicionando estilizacao

--- src/App.vue
+++ src/App.vue
@@ -3,16 +3,8 @@
     <div class="column is-one-quarter">
       <BarraLateral @aoTemaAlterado="trocarTema"/>
     </div>
-
     <div class="column is-three-quarter conteudo">
-      <Formulario @aoSalvarTarefa="salvarTarefa"/>
-
-      <div class="lista">
-        <Tarefa v-for="(tarefa, index) in tarefas" :key="index" :tarefa="tarefa"/>
-        <Box v-if="listaEstavazia">
-          Você não esta muito produtivo hoje :(
-        </Box>
-      </div>
+      <router-view></router-view>
     </div>
   </main>
 </template>
@@ -20,34 +12,19 @@
 <script lang="ts">
 import { defineComponent } from "vue";
 import BarraLateral from "./components/BarraLateral.vue";
-import Formulario from "./components/Formulario.vue";
-import Tarefa from "./components/Tarefa.vue";
-import ITarefa from "./interfaces/ITerafa";
-import Box from "@/components/Box.vue"
+
  
 export default defineComponent({
   name: "App",
   components: {
     BarraLateral,
-    Formulario,
-    Tarefa,
-    Box,
   },
   data () {
     return {
-      tarefas: [] as ITarefa[],
       modoEscuroAtivo: false
     }
   },
-  computed: {
-    listaEstavazia () : boolean {
-      return this.tarefas.length === 0 
-    }
-  },
   methods: {
-    salvarTarefa (tarefa: ITarefa) {
-      this.tarefas.push(tarefa)
-    },
     trocarTema (modoEscuroAtivo: boolean) {
       this.modoEscuroAtivo = modoEscuroAtivo
     }
