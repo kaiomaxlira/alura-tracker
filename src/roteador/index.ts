a2e6b9b28f81697a4a24eedea7de3bfce258d04b
Kaio Max <kaiomax01@gmail.com>
2024-02-26 10:33:08 -0300

    Adicionando rotas de novo projeto e editando projeto

--- src/roteador/index.ts
+++ src/roteador/index.ts
@@ -1,6 +1,7 @@
 import { RouteRecordRaw, createRouter, createWebHashHistory } from "vue-router";
 import Tarefas from '../views/Tarefas.vue'
 import Projetos from '../views/Projetos.vue'
+import Formulario from '../views/Projetos/Formulario.vue'
 
 const rotas: RouteRecordRaw[] = [{
     path: '/',
@@ -13,9 +14,14 @@ const rotas: RouteRecordRaw[] = [{
     component: Projetos
 },
 {
-    path: '/projetos',
-    name: '/Projetos',
-    component: Projetos
+    path: '/projetos/novo',
+    name: '/Novo projetos',
+    component: Formulario
+},
+{
+    path: '/projetos/:id',
+    name: '/Editar projetos',
+    component: Formulario
 },
 ]
    
